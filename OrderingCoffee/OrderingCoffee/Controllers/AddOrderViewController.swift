1a8229227870154ccae8fa4f5186bba6e9490608
Damagucci-Juice <jsr9874@gmail.com>
2023-01-11 20:34:15 +0900

    feat: coffee type 테이블뷰에 띄움

--- OrderingCoffee/OrderingCoffee/Controllers/AddOrderViewController.swift
+++ OrderingCoffee/OrderingCoffee/Controllers/AddOrderViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class AddOrderViewController: UIViewController {
+final class AddOrderViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private var segmentControl: UISegmentedControl!
+    private var vm = AddCoffetOrderViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension AddOrderViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AddOrderTableViewCell", for: indexPath)
+        cell.textLabel?.text = vm.types[indexPath.row]
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vm.types.count
+    }
+}
--- OrderingCoffee/OrderingCoffee/ViewModels/AddCoffetOrderViewModel.swift
+++ OrderingCoffee/OrderingCoffee/ViewModels/AddCoffetOrderViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct AddCoffetOrderViewModel {
+    var neme: String?
+    var email: String?
+    var selectedType: String?
+    var selectedSize: String?
+    
+    var types: [String] {
+        CoffeeType.allCases.map { $0.rawValue.capitalized }
+    }
+    var sizes: [String] {
+        CoffeeSize.allCases.map { $0.rawValue.capitalized }
+    }
+}
