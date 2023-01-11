dca724e1ab0878be91cdf9f44eacf82289af215f
Damagucci-Juice <jsr9874@gmail.com>
2023-01-11 19:54:37 +0900

    feat: Models, Order, CoffeeType, CoffeeSize

---
+++ OrderingCoffee/OrderingCoffee/Models/CoffeeType.swift
@@ -0,0 +1,15 @@
+//
+//  CoffeeType.swift
+//  OrderingCoffee
+//
+//  Created by YEONGJIN JANG on 2023/01/11.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable, CaseIterable {
+    case latte
+    case americano
+    case cappuccino
+    case frappuccino
+}
