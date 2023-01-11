8b83895210465595b3b7c8d4e697336b28384f8c
Damagucci-Juice <jsr9874@gmail.com>
2023-01-11 20:11:35 +0900

    feat: VMs

---
+++ OrderingCoffee/OrderingCoffee/ViewModels/OrderListViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  OrderListViewModel.swift
+//  OrderingCoffee
+//
+//  Created by YEONGJIN JANG on 2023/01/11.
+//
+
+import Foundation
+
+struct OrderListViewModel {
+    private var orderViewModels: [OrderViewModel]
+    
+    init(_ orderViewModels: [OrderViewModel] = []) {
+        self.orderViewModels = orderViewModels
+    }
+}
+
+extension OrderListViewModel {
+    subscript(_ index: Int) -> OrderViewModel {
+        orderViewModels[index]
+    }
+    
+    var count: Int { orderViewModels.count }
+    
+    mutating func updateOrderViewModels(_ vm: [OrderViewModel]) {
+        self.orderViewModels = vm
+    }
+}
