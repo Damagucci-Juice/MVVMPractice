1b38c3fb5460e6b0f127e9f5b4efc3784858431a
Damagucci-Juice <jsr9874@gmail.com>
2023-01-11 11:19:47 +0900

    init: OrderingCoffe App

---
+++ OrderingCoffee/OrderingCoffee/Controllers/AddOrderViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddOrderViewController.swift
+//  OrderingCoffee
+//
+//  Created by YEONGJIN JANG on 2023/01/11.
+//
+
+import UIKit
+
+class AddOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OrderingCoffee/OrderingCoffee/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  OrdersTableViewController.swift
+//  OrderingCoffee
+//
+//  Created by YEONGJIN JANG on 2023/01/11.
+//
+
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+    
+}
