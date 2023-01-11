af4c41c9a107596106715bc83c8b55868b5c8c1c
Damagucci-Juice <jsr9874@gmail.com>
2023-01-11 20:02:18 +0900

    feat: Webservice Testing

--- OrderingCoffee/OrderingCoffee/Controllers/OrdersTableViewController.swift
+++ OrderingCoffee/OrderingCoffee/Controllers/OrdersTableViewController.swift
@@ -8,5 +8,21 @@
 import UIKit
 
 class OrdersTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchOrders()
+    }
     
+    private func fetchOrders() {
+        guard let orderUrl = URL(string: "https://warp-wiry-rugby.glitch.me/orders") else { return }
+        let resource = Resource<[Order]>(baseURL: orderUrl)
+        WebService().load(resource: resource) { result in
+            switch result {
+            case .success(let orders):
+                print(orders)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
