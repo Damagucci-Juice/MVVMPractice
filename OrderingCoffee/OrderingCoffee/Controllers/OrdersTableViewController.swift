b8a8c49e7215d3f52d8818203b452a489c1b1b8e
Damagucci-Juice <jsr9874@gmail.com>
2023-01-11 20:23:58 +0900

    feat: post한 데이터가 OrderViewController 에서 확인됨

--- OrderingCoffee/OrderingCoffee/Controllers/OrdersTableViewController.swift
+++ OrderingCoffee/OrderingCoffee/Controllers/OrdersTableViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class OrdersTableViewController: UITableViewController {
+    
+    private var orderListViewModel = OrderListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchOrders()
@@ -16,13 +19,32 @@ class OrdersTableViewController: UITableViewController {
     private func fetchOrders() {
         guard let orderUrl = URL(string: "https://warp-wiry-rugby.glitch.me/orders") else { return }
         let resource = Resource<[Order]>(baseURL: orderUrl)
-        WebService().load(resource: resource) { result in
+        WebService().load(resource: resource) { [unowned self] result in
             switch result {
             case .success(let orders):
-                print(orders)
+                self.orderListViewModel.updateOrderViewModels(orders.map { OrderViewModel(order: $0) })
+                self.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
+    }
+}
+
+extension OrdersTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        orderListViewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let vm = orderListViewModel[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderTableViewCell", for: indexPath)
+        cell.textLabel?.text = vm.type
+        cell.detailTextLabel?.text = vm.size
+        return cell
     }
 }
---
+++ OrderingCoffee/OrderingCoffee/ViewModels/AddCoffetOrderViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddCoffetOrderViewModel.swift
+//  OrderingCoffee
+//
+//  Created by YEONGJIN JANG on 2023/01/11.
+//
+
+import Foundation
